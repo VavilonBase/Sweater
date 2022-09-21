e1ea8680a32216957413c6da4a2e66d294669dbf
VavilonBase <VavilonBase@mail.ru>
2022-09-21 10:29:26 +0300

    Migrate from mustache to freemaker

--- src/main/java/com/sweater/sweater/controllers/MainController.java
+++ src/main/java/com/sweater/sweater/controllers/MainController.java
@@ -5,6 +5,7 @@ import com.sweater.sweater.entities.User;
 import com.sweater.sweater.repositories.MessageRepo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,26 +13,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     private final MessageRepo messageRepo;
 
-    public GreetingController(MessageRepo messageRepo) {
+    public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
     }
 
     @GetMapping("/")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Map<String, Object> model) {
-        model.put("name", name);
+                           Model model) {
+        model.addAttribute("name", name);
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
 
-        Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+        Iterable<Message> messages;
+
+        if (!filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
 
         return "main";
     }
@@ -47,21 +55,6 @@ public class GreetingController {
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    @PostMapping("/filter")
-    public String filterMessages(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
         model.put("messages", messages);
 
         return "main";
